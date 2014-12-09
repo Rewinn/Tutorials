c9d5607fc735b0996ef91f27d9062baa2e582842
rewinn <rachel.winn@me.com>
2014-12-08 20:10:13 -0600

    Updated code. Still not working / @mrcwinn

--- Clock/clock.js
+++ Clock/clock.js
@@ -9,17 +9,13 @@ function updateClock() {
     var degree_minutes = -90 + (minutes * (360 / 6));
     var degree_hour    = -90 + (hours * (360 / 12));
 
-    $('.hour').css('-webkit-transform', 'rotate(' + degree_hour + ' deg)');
-    $('.minute').css('-webkit-transform', 'rotate(' + degree_minutes + ' deg)');
-    $('.second').css('-webkit-transform', 'rotate(' + degree_seconds + ' deg)');
+    $('.hour').css('-webkit-transform','rotate('+degree_hour+'deg)');
+    $('.minute').css('-webkit-transform','rotate('+degree_minutes+'deg)');
+    $('.second').css('-webkit-transform','rotate('+degree_seconds+'deg)');
 }
 
 setInterval(updateClock, 1000);
 updateClock();
-
-
-
-
 
 
 
