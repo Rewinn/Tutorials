8bd60c813fae659ce8d273a9d55ec01b9ef095a7
rewinn <rachel.winn@me.com>
2014-12-15 14:03:56 -0600

    Counter Working
    @mrcwinn

--- Button Counter/js/button.js
+++ Button Counter/js/button.js
@@ -1,7 +1,7 @@
 /*jslint browser:true */
 
-function modify_qty (val) {
-	var qty     = document.getElementById('quantity').value;
+function modify_qty(val) {
+	var qty     = document.getElementById('qty').value;
 	var new_qty = parseInt(qty,10) + val;
 
 	if (new_qty < 0) {
