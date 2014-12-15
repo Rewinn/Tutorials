21fddd6e3bb2cdf84af80737fbe6f506e7a8b16c
rewinn <rachel.winn@me.com>
2014-12-15 13:54:35 -0600

    Starting the JS
    @mrcwinn

--- Button Counter/js/button.js
+++ Button Counter/js/button.js
@@ -1,4 +1,16 @@
+/*jslint browser:true */
 
+function modify_qty (val) {
+	var qty     = document.getElementById('quantity').value;
+	var new_qty = parseInt(qty,10) + val;
+
+	if (new_qty < 0) {
+		new_qty = 0;
+	}
+
+	 document.getElementById('qty').value = new_qty;
+     return new_qty;
+}
 
 
 
