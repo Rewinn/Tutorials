bc35a74a6da7f059a1b5f737c48ff6e4929bcc1b
rewinn <rachel.winn@me.com>
2014-12-18 12:44:39 -0600

    @mrcwinn Updated but not working

--- Button Counter/js/button.js
+++ Button Counter/js/button.js
@@ -3,16 +3,21 @@
 function downCounter () {
 	var subtract = document.getElementById("minus");
 	if (this.value <= 0) {
-		subtract === 0;
+		subtract = 0;
 	}	else {
-		subtract == -1;
+		subtract = -1;
 	}
 }
 
+var clickDown = document.getElementById("minus");
+clickDown.addEventListener("click", downCounter, false);
 
 function upCounter () {
 	var addition = document.getElementById("plus");
 	if (this.value >= 0) {
-		addition == +1;
+		addition = +1;
+	}
 }
 
+var clickUp = document.getElementById("plus");
+clickUp.addEventListener("click", upCounter, false);
