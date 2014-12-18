845dac34a99d2874c7836d90f533be721f6ef88c
rewinn <rachel.winn@me.com>
2014-12-18 12:34:21 -0600

    @mrcwinn
    I still need to add my event listener, I think.

--- Button Counter/js/button.js
+++ Button Counter/js/button.js
@@ -1,48 +1,18 @@
 /*jslint browser:true */
 
-function modify_qty(val) {
-	var qty     = document.getElementById('qty').value;
-	var new_qty = parseInt(qty,10) + val;
-
-	if (new_qty < 0) {
-		new_qty = 0;
+function downCounter () {
+	var subtract = document.getElementById("minus");
+	if (this.value <= 0) {
+		subtract === 0;
+	}	else {
+		subtract == -1;
 	}
-
-	 document.getElementById('qty').value = new_qty;
-     return new_qty;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var add = 0;
-
-function onClick() {
-	add+=1;
-	document.getElementById("clicks").innerHTML = clicks;
-} 
-
-
-var subtract = add - 1;
-
-function onClick() {
-	subtract;
-	document.getElementById("clicks").innerHTML = clicks;
-};
-*/
+function upCounter () {
+	var addition = document.getElementById("plus");
+	if (this.value >= 0) {
+		addition == +1;
+}
 
